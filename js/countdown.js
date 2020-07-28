1de4ad1d6da134a1f36190d76d4a975f7572cc48
CHKDIN INDIA <68904809+chkdinind@users.noreply.github.com>
2020-07-28 17:05:34 +0530

    New Update

---
+++ js/countdown.js
@@ -0,0 +1,51 @@
+function getTimeRemaining(endtime) {
+  
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  
+  // To calculate the time difference of two dates 
+var Difference_In_Time = endtime.getTime() - new Date().getTime(); 
+  
+// To calculate the no. of days between two dates 
+var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24); 
+
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  console.log(Difference_In_Days);
+  console.log(days);
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  console.log(endtime);
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(new Date('2020-09-09'));
+initializeClock('clockdiv', deadline);
